d1c18fff567aa1bd3f8003c1873d6e5d2dd84643
Anasmemon670 <memonanas676@gmail.com>
2025-03-14 14:31:31 +0500

    Calcu

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 let userInput = document.getElementById("Input-Calculator");
 let FinalResult = "";
 
-
-function appendNumber(Number){
+function appendNumber(Number) {
     FinalResult += Number;
     userInput.value = FinalResult;
 }
-function appendOperator(Operator){
+function appendOperator(Operator) {
     FinalResult += " " + Operator + " ";
     userInput.value = FinalResult;
 }
-function clearDisplay(){
-    FinalResult  = "";
+function clearDisplay() {
+    FinalResult = "";
     userInput.value = "";
 }
-function DeletOneItemToDisplay(){
-    // let arr = [];
-    // for(let i = 0; i < arr.length; i++){
-    //     FinalResult += arr.pop();
-    //      userInput.value = FinalResult;
-    // } 
-    FinalResult = userInput.value.tostring().slice(0 , -1);
-      
-   
+function DeletOneItemToDisplay() {
+    FinalResult = FinalResult.slice(0, -1);
+    userInput.value = FinalResult
 }
+
+function CalculateFinalResult() {
+    try {
+        userInput.value = eval(FinalResult);
+    }
+    catch {
+        userInput.value = "Error";
+    }
+}
+
